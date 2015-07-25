84c7b70b741c4eaeaf158d00ba1bd74838ac6087
marte3707 <marco.cantarero1@gmail.com>
2015-07-25 19:36:00 +0200

    homepage v.0.1

--- new_index.php
+++ new_index.php
@@ -17,7 +17,6 @@
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/new_main.css">
 	<link rel="stylesheet" href="jquery-ui/jquery-ui.min.css">
-
 </head>
 	
 <body>
@@ -54,14 +53,14 @@
 		</div>
 	</nav>
 
-
+	<div id="bg"></div>
 	<br>
 	<br>
 	<br>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-lg-8 col-sm-8" id="regForm">
-				<form action="php/remote.php" method="post">
+			<div class="col-md-8 col-lg-8 col-sm-8" id="form1" >
+				<form action="php/remote.php" method="post" id="">
 					<p class="lead">REGISTRATI GRATUITAMENTE.</p>
 					<div class="form-group">
 						<label for="inputName">Nome: </label>
@@ -73,10 +72,10 @@
 					</div>
 					<div class="radio">
 						<label class="radio-inline" for="gender">
-							<input type="radio" name="genderRadio" value="m" checked="true" />M
+							<input type="radio" name="genderRadio" value="male" checked="true" />M
 						</label>
 						<label class="radio-inline" for="gender">
-							<input type="radio" name="genderRadio" value="f" />F
+							<input type="radio" name="genderRadio" value="female" />F
 						</label>
 					</div>
 					<div class="form-group">
@@ -88,12 +87,12 @@
 							<input type="password" class="form-control" id="inputPassword" placeholder="Password" name="inputPassword">
 					</div>
 					<div class="form-group">
-						<label for="inputRepeatPassword">Password: </label>
+						<label for="inputRepeatPassword">Repeat password: </label>
 							<input type="password" class="form-control" id="inputRepeatPassword" placeholder="Repeat Password" name="inputRepeatPassword">
 					</div>
-					<input type="checkbox"> Accetto l'informativa.
+					<input type="checkbox" class="move"> <p class="move move2">Accetto l'informativa.</p>
 					<br>
-					<button type="submit" class="btn btn-default" id="sendForm">Registrati</button>
+					<button type="submit" class="btn btn-default move" id="sendForm">Registrati</button>
 				</form>
 				<div id="dialog" title="Warning!"></div>
 			</div>
